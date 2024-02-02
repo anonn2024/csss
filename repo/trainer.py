cefcb6ea584f3141128bef8be1893bbfc01c0746
anonn2024 <158552014+anonn2024@users.noreply.github.com>
2024-02-02 10:05:37 +0200

    Update trainer.py

--- repo/trainer.py
+++ repo/trainer.py
@@ -8,8 +8,8 @@ from utils import *
 from data import DataContainer
 import fairtorch
 
-# NOTICE : Originaly in this work, X was used as the group feature and Z was used as the remaining features and thats how they are named in the code.
-# This is the opposite of how they are denoted in the submitted paper.
+# NOTICE: In our code, we use X to denote group features; in the paper, groups were denoted by Z, and the notation X was used to describe all features. 
+# We apologize for any confusion this may raise while revising the code, and will correct this so that notation will be consistent for the next revision and before we make our code public.
 
 class StrategicTrainer:
     def __init__(self, model, data: DataContainer, opt, loss,
